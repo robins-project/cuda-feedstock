1149378a5fe2f866c8b339f0202b0897864c11a5
v-turin <vladimir.turin@opencascade.com>
2018-10-01 13:27:01 +0300

    feature_cuda

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,15 +1,18 @@
+from shutil import which
 import subprocess
 import os
 
 def nvcc():
     "checks nvcc version"
-    lines = subprocess.check_output(['nvcc', '--version']).decode('utf-8').splitlines()
-    res = False
-    for line in lines:
-        if "release "+os.environ['PKG_VERSION'] in line:
-            res = True
-            break
+    version = "0"
+    if which('nvcc') is not None:
+        lines = subprocess.check_output(['nvcc', '--version']).decode('utf-8').splitlines()
+        for line in lines:
+            if line.startswith("Cuda compilation tools"):
+                # Cuda compilation tools, release 9.2, V9.2.148
+                version = line.split()[-1][1:]
+                break
 
-    return res
+    return version
 
-assert nvcc()
+assert nvcc() == os.environ['PKG_VERSION']
